1e23116b4c883481287d2f47d4a115c5fe8a2ab7
Sumit K <synergy2411@outlook.com>
2022-03-22 15:30:29 +0530

    html file served

--- 02-node-basics/modules-demo/core-module.js
+++ 02-node-basics/modules-demo/core-module.js
@@ -17,17 +17,64 @@
 
 // console.log("Extension : ", path.extname(url));
 
+// Access the local file system
+// const fs = require("fs");
 
-const fs = require("fs");
-
-fs.readFile("./files/demo.md",(err, data) => {
-    if(err){
-        console.log(err);
-    }
-    console.log("ASYNC READ -> ", data.toString());
-})
+// fs.readFile("./files/demo.md",(err, data) => {
+//     if(err){
+//         console.log(err);
+//     }
+//     console.log("ASYNC READ -> ", data.toString());
+// })
 
 
-const content = fs.readFileSync("./files/demo.md");
+// const content = fs.readFileSync("./files/demo.md");
 
-console.log("Sync Data -> ", content.toString());           // Buffer Data
+// console.log("Sync Data -> ", content.toString());           // Buffer Data
+
+
+// const fs = require("fs").promises;
+
+// fs.readFile("./files/demo.md")
+//     .then(content => {
+//         fs.writeFile("./files/new-file.md",content)
+//             .then(result => console.log(result))
+//             .catch(console.log)
+//         // console.log(content.toString());
+//     })
+//     .catch(console.log)
+
+
+
+
+
+const http = require("http");
+const fs = require("fs").promises;
+
+const requestListener = (request, response) => {
+
+    // response.setHeader("Content-Type", "application/json")
+    response.setHeader("Content-Type", "text/html")
+    response.writeHead(200);
+    // response.end("Hello from Server")
+    // response.end(JSON.stringify({message : "SUCCESS"}))
+    
+    fs.readFile("./files/index.html")
+        .then(content => {
+            response.end(content)
+        }).catch(erro => {
+            response.end("<p>Error Occured</p>")
+        })
+
+}
+
+const server = http.createServer(requestListener);
+
+server.listen(9000, () => console.log("HTTP Server started at PORT : 9000"))
+
+
+
+
+
+
+
