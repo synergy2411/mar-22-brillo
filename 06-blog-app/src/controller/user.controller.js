aa973722711a67be8e0dcab5b7f14c3967c0ce7d
Sumit K <synergy2411@outlook.com>
2022-03-25 15:58:05 +0530

    password encypted

--- 06-blog-app/src/controller/user.controller.js
+++ 06-blog-app/src/controller/user.controller.js
@@ -1,10 +1,13 @@
 const UserModel = require("../model/user.model");
+const bcrypt = require("bcryptjs");
 
 const getUsers = () => {};
 
 const createUser = async (req, res) => {
   try {
-    const user = new UserModel(req.body);
+    const { password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 12);
+    const user = new UserModel({ ...req.body, password: hashedPassword });
     const createdUser = await user.save();
     return res.send(createdUser);
   } catch (err) {
