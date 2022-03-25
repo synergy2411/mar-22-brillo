aa5210da83b51a4f42ee30178da0d03c3def2a40
Sumit K <synergy2411@outlook.com>
2022-03-25 16:54:05 +0530

    user model added

--- 06-blog-app/src/routes/user.routes.js
+++ 06-blog-app/src/routes/user.routes.js
@@ -8,4 +8,6 @@ UserRouter.route("/")           // http://localhost:9090/api/users
     .post(createUser)
 UserRouter.route("/login")                                // http://localhost:9090/api/users/login -> POST
     .post(onUserLogin)
+// UserRouter.route("/:userId/posts/:postId")        
+// http://localhost:9090/api/users/actualUserId/posts/actualPostId
 module.exports = UserRouter;
