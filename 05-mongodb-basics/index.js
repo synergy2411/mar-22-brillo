7de981ed6f1664929eede1ecd555257eaad00040
Sumit K <synergy2411@outlook.com>
2022-03-25 11:29:27 +0530

    CRUD with mongodb

--- 05-mongodb-basics/index.js
+++ 05-mongodb-basics/index.js
@@ -1,26 +1,39 @@
 const { MongoClient } = require("mongodb");
 
-const MONGO_URI ="mongodb://localhost:27017";
+const MONGO_URI = "mongodb://localhost:27017";
 
-const client = new MongoClient(MONGO_URI)
+const client = new MongoClient(MONGO_URI);
 
-client.connect()
-    .then(conn => {
-        console.log("Mongo Connected")
-        const db = client.db("brillio-db")
-        const postsCollection = db.collection("posts")
+client
+  .connect()
+  .then((conn) => {
+    console.log("Mongo Connected");
+    const db = client.db("brillio-db");
+    const postsCollection = db.collection("posts");
 
-        
-        // READ
-        postsCollection.find().toArray().then(documents =>{console.log(documents)}).catch(console.log)
-        
-        // CREATE
-        // postsCollection.insertOne({title : "grocery", body : "to buy some pulses"})
-        //     .then(result => {
-        //         console.log(result)
-        //     }).catch(console.log)
+    // UPDATE
+    // postsCollection.updateOne({title : "grocery"}, {$set : { body : "to buy wheat"}})
+    //     .then(result => console.log(result)).catch(console.log)
 
-    }).catch(console.log)
+    // DELETE
+    // postsCollection.deleteOne({title : "angular"})
+    //     .then(result => console.log(result)).catch(console.log)
 
+    // READ
+    postsCollection
+      .find()
+      .toArray()
+      .then((documents) => {
+        console.log(documents);
+      })
+      .catch(console.log);
 
-    // nodemon index.js
+    // CREATE
+    // postsCollection.insertOne({name : "john", age : 32})
+    //     .then(result => {
+    //         console.log(result)
+    //     }).catch(console.log)
+  })
+  .catch(console.log);
+
+// nodemon index.js
