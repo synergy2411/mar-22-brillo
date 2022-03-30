eb14d94e45b12817d265411389a2fa0af51cb925
Sumit K <synergy2411@outlook.com>
2022-03-30 16:47:44 +0530

    token assigned

--- 08-jwt-basics/src/server.js
+++ 08-jwt-basics/src/server.js
@@ -2,13 +2,22 @@ const express = require("express");
 require("./db");
 const UserModel = require("./model/user.model");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { response } = require("express");
 
 const app = express();
+const SECRET_KEY = "MY_SUPER_SECRET_KEY";
+
 app.use(express.json());
 app.use(express.static(__dirname+"/public"));
 
+
 app.get("/login", (req, res) => {
     res.sendFile(__dirname+"/public/login.html");
+})
+
+app.get("/user/profile", (req, res)=>{
+    res.sendFile(__dirname+"/public/profile.html")
 })
 
 app.post("/user/login", async (req, res) => {
@@ -18,7 +27,16 @@ app.post("/user/login", async (req, res) => {
         if(foundUser){
             const isMatch = await bcrypt.compare(password, foundUser.password);
             if(isMatch){
-                return res.send({message : "User is Authenticated"})
+                try{
+                    const token = await jwt.sign({
+                        id : foundUser._id,
+                        email : email
+                    }, SECRET_KEY)
+                    return res.send({message : "SUCCESS", status : 200, token : token})
+                }catch(err){
+                    console.log(err);
+                    return res.send(err)
+                }
             }else{
                 return res.send({message : "Bad Credentials"})
             }
