443409b8116490897db15f2d0ac7b11440b47d49
Sumit K <synergy2411@outlook.com>
2022-03-30 14:51:39 +0530

    user verified from db

--- 08-jwt-basics/src/public/scripts/main.js
+++ 08-jwt-basics/src/public/scripts/main.js
@@ -1,11 +1,28 @@
 window.onload = () => {
     const btnSignup = document.getElementById("btnSignup")
+    const btnLogin = document.getElementById("btnLogin")
     const txtEmail = document.getElementById("txtEmail")
     const txtPassword = document.getElementById("txtPassword")
 
+    btnLogin.addEventListener("click", event => {
+        event.preventDefault();
+        fetch("http://localhost:9090/user/login",{
+            method : "POST",
+            headers : {
+                "Content-Type" : "application/json"
+            },
+            body: JSON.stringify({
+                email : txtEmail.value,
+                password : txtPassword.value
+            })
+        })
+        .then(response => response.json())
+        .then(result => console.log(result))
+    })
+
     btnSignup.addEventListener("click", (event) => {
         event.preventDefault();
-        fetch("/signup",{
+        fetch("http://localhost:9090/signup",{
             method : "POST",
             headers : {
                 "Content-Type" : "application/json"
--- 08-jwt-basics/src/server.js
+++ 08-jwt-basics/src/server.js
@@ -11,11 +11,31 @@ app.get("/login", (req, res) => {
     res.sendFile(__dirname+"/public/login.html");
 })
 
+app.post("/user/login", async (req, res) => {
+    const { email, password } = req.body;
+    try{
+        const foundUser = await UserModel.findOne({email : email});
+        if(foundUser){
+            const isMatch = await bcrypt.compare(password, foundUser.password);
+            if(isMatch){
+                return res.send({message : "User is Authenticated"})
+            }else{
+                return res.send({message : "Bad Credentials"})
+            }
+        }else{
+            return res.send({message : "User Not found"})
+        }
+    }catch(err){
+        console.log(err)
+        return res.send(err)
+    }
+})
+
 app.post("/signup", async (req, res) => {
     const {password} = req.body;
     try{
         const hashedPassword = await bcrypt.hash(password, 12);
-        const user = new UserModel({...req.body, password : hashedPassword})
+        const user = new UserModel({...req.body, password : hashedPassword})    // {email : "", password :""}
         const createdUser = await user.save()
         return res.send({
             message  :"User created successfully", 
