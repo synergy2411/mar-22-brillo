4a671862bc7b18ba372758c49a2d14f7308a9001
Sumit K <synergy2411@outlook.com>
2022-04-01 14:42:25 +0530

    ejs layout added

---
+++ 12-express-ejs/src/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const ejs = require("ejs");
+const expEjsLayouts = require("express-ejs-layouts");
+
+const app = express();
+
+app.use(expEjsLayouts);
+app.set('layout', './layouts/layout');
+app.set("view engine", "ejs");
+
+app.get("/views/welcome", (req, res) => {
+    res.render("welcome", {
+        username : "asfiya"
+    });
+})
+
+app.listen(9090, () => console.log("Server Started at PORT : 9090"))
